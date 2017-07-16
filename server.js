d30dadb090ccc484cb18815b3d7f6c557399da70
Shery11 <laughnplay>
2017-07-17 02:41:48 +0500

    added post route

--- server.js
+++ server.js
@@ -66,13 +66,51 @@ app.delete('/todo/:id',function(req,res){
   
   if(matchedTodo){
       todo = _.without(todo,matchedTodo);
-      
-
-      console.log(todo);
       res.json(todo);
     }else{
       res.status(404).send();
     }
+
+});
+
+
+app.put('/todo/:id',function(req,res){
+  
+  var body = req.body;
+  var todoId = parseInt(req.params.id,10);
+  var matchedTodo = _.findWhere(todo,{ id : todoId}); 
+  body = _.pick(body,'completed', 'description')
+  var validAttributes = {};
+
+  if(!matchedTodo){
+    return res.status(404).send();
+  }
+
+
+  if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+     
+     validAttributes.completed = body.completed;
+  }else if(body.hasOwnProperty('completed')){
+        return res.status(400).send
+  }else{
+
+  }
+   
+
+  if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+     validAttributes.description = body.description;
+  }else if(body.hasOwnProperty('description')){
+        return res.status(400).send
+  }
+
+
+
+  _.extend(matchedTodo,validAttributes);
+
+  res.json(matchedTodo);
+
+    
+
 
 })
 
